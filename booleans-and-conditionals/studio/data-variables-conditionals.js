b970b07152f50eeba768972702de416f73a85c79
Quombus <beckettrichardmatthew@gmail.com>
2024-06-12 10:38:59 -0500

    LaunchCode made me do it

--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -19,27 +19,49 @@ let prepareForLiftOff = true;
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
 if (astronautCount > 7) {
     prepareForLiftOff = false;
+    console.log("Too many fellas on the ship.");
+} else {
+    console.log("We have the appropriate amount of astronauts!");
 }
 // add logic below to verify all astronauts are ready
 if (astronautStatus) {
     prepareForLiftOff = true;
+    console.log("Astronauts are good to go!");
+} else {
+    prepareForLiftOff = false
+    console.log("Not all astronauts are good to go.");
 }
 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
 if (totalMassKg > 850000) {
     prepareForLiftOff  = false;
+    console.log("The mass is too darn high.");
+} else {
+console.log("We have an acceptable mass!");
 }
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
 if (fuelTempCelsius > minimumFuelTemp && fuelTempCelsius < maximumFuelTemp) {
 prepareForLiftOff = true;
+console.log("Just the right temperature!");
+} else {
+    prepareForLiftOff = false;
+    console.log("Temps are all whack.");
 }
 // add logic below to verify the fuel level is at 100%
 if (fuelLevel === "100%") {
     prepareForLiftOff = true;
+    console.log("We have enough juice!")
+} else {
+    prepareForLiftOff = false;
+    console.log("Not enough juice.");
 }
 // add logic below to verify the weather status is clear
 if (weatherStatus === "clear") {
     prepareForLiftOff = true;
+    console.log("Weather is acceptable for launch!");
+} else {
+    prepareForLiftOff = false;
+    console.log("Weather conditions are not suitable for take-off.");
 }
 // Verify shuttle launch can proceed based on above conditions
 if (prepareForLiftOff){
