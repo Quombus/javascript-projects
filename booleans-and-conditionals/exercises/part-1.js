b588c728289e1f514ec3bdb46c20664d7755245e
Quombus <beckettrichardmatthew@gmail.com>
2024-06-12 10:28:31 -0500

    LaunchCode made me do it

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,13 +1,12 @@
 // Declare and initialize the variables for exercise 1 here:
-
-// BEFORE running the code, predict what will be printed to the console by the following statements:
-
 let engineIndicatorLight = "red blinking";
 let spaceSuitsOn = true;
-let shuttleCabinsReady = true;
-let crewStatus = spaceSuitsOn && shuttleCabinsReady;
+let shuttleCabinReady = true;
+let crewStatus = spaceSuitsOn && shuttleCabinReady;
 let computerStatusCode = 200;
 let shuttleSpeed = 15000;
+
+// BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
    console.log("engines have started");
@@ -17,25 +16,8 @@ if (engineIndicatorLight === "green") {
    console.log("engines are off");
 }
 
-if (crewStatus){
-    console.log("Crew Ready");
-}else {
-   console.log("Crew Not Ready");
-}
 
-if (computerStatusCode === 200){
-   console.log("Please Stand By. Computer is Rebooting")
-} else if (computerStatusCode === 400) {
-   console.log("Success! Computer online");
-} else {
-   console.log("Alert! Computer Offline")
-}
 
-if (shuttleSpeed > 17500){
-   console.log("ALERT! Escape velocity reached");
-} else if (shuttleSpeed < 8000){
-   console.log("ALERT! Cannot maintain orbit!");
-} else {
-   console.log("Stable Speed");
-}
+
+
 
--- booleans-and-conditionals/studio/data-variables-conditionals.js
+++ booleans-and-conditionals/studio/data-variables-conditionals.js
@@ -1,15 +1,49 @@
 // Initialize Variables below
+let date = "Monday 2019-03-18";
+let time = "10:05:34 AM";
+let astronautCount = 7;
+let astronautStatus = "ready";
+let averageAstronautMassKg = "80.7";
+let crewMassKg = astronautCount * averageAstronautMassKg;
+let fuelMassKg = 760000;
+let shuttleMassKg = 74842.31;
+let totalMassKg = crewMassKg + fuelMassKg + shuttleMassKg;
+let maximumMassLimit = 850000;
+let fuelTempCelsius = -225;
+let minimumFuelTemp = -300;
+let maximumFuelTemp = -150;
+let fuelLevel = "100%"
+let weatherStatus = "clear";
+let prepareForLiftOff = true;
 
 // add logic below to verify total number of astronauts for shuttle launch does not exceed 7
-
+if (astronautCount > 7) {
+    prepareForLiftOff = false;
+}
 // add logic below to verify all astronauts are ready
+if (astronautStatus) {
+    prepareForLiftOff = true;
+}
 
 // add logic below to verify the total mass does not exceed the maximum limit of 850000
-
+if (totalMassKg > 850000) {
+    prepareForLiftOff  = false;
+}
 // add logic below to verify the fuel temperature is within the appropriate range of -150 and -300
-
+if (fuelTempCelsius > minimumFuelTemp && fuelTempCelsius < maximumFuelTemp) {
+prepareForLiftOff = true;
+}
 // add logic below to verify the fuel level is at 100%
-
+if (fuelLevel === "100%") {
+    prepareForLiftOff = true;
+}
 // add logic below to verify the weather status is clear
-
+if (weatherStatus === "clear") {
+    prepareForLiftOff = true;
+}
 // Verify shuttle launch can proceed based on above conditions
+if (prepareForLiftOff){
+    console.log("Clear to launch!");
+} else {
+    console.log("NOT clear to launch.")
+}
