3fd9a06f4730ed7b2f37a11b2fbc03b7f0341957
Quombus <beckettrichardmatthew@gmail.com>
2024-06-27 09:50:04 -0500

    efficency

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -6,6 +6,7 @@ b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
 
+
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
@@ -13,3 +14,15 @@ c) Print the new array to confirm your work.
 */
 
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+function itemAlteration(param){
+   return typeof param == typeof 1 ? param = param * 3: param = "AAAAAAH!";
+}
+console.log(arr.map(e => itemAlteration(e)));
+
+
+
+
+
+
+
+
